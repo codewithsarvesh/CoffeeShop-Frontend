ee293b2c9208c31c54df61b8934d83b35bea5c8f
codewithsarvesh <143922159+codewithsarvesh@users.noreply.github.com>
2024-04-01 13:10:22 +0530

    menu contact done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,10 +2,29 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      This is Contact page..
-    </div>
-  )
+    <section className="contact" id="contact">
+      <h1 className="heading"> <span>contact</span> us </h1>
+      <div className="row">
+        <iframe className="map" src="https://maps.app.goo.gl/1BK8JyuEFiJe42at5" allowFullScreen="" loading="lazy"></iframe>
+        <form action="">
+          <h3>get in touch</h3>
+          <div className="inputBox">
+            <span className="fas fa-user"></span>
+            <input type="text" placeholder="name" />
+          </div>
+          <div className="inputBox">
+            <span className="fas fa-envelope"></span>
+            <input type="email" placeholder="email" />
+          </div>
+          <div className="inputBox">
+            <span className="fas fa-phone"></span>
+            <input type="number" placeholder="number" />
+          </div>
+          <input type="submit" value="contact now" className="btn" />
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,49 +1,65 @@
 import React from 'react';
-
+import img1 from './images/Mocha-Coffee.jpg';
+import img2 from './images/cappucino-fotoliastock.7f1e032b.jpg';
+import img3 from './images/cold-frappe-coffee-cream-gray-background-92379178.webp';
+import img4 from './images/Blackcoffee.jpg';
+import img5 from './images/keto-chocolate-chip-cookies-2.jpg';
+import img6 from './images/Butterbun.jpg';
 const Menu = () => {
   return (
-    <>
-    <div>
-      
-          <div className="box">
-            <i className="fas fa-trash"></i>
-            <img src="images/cart-1.png" alt="" />
-            <div className="content">
-              <h3>mocha magic</h3>
-              <span className="price">$34.99/=</span>
-              <span className="quantity"> qty: 1</span>
-            </div>
-          </div>
-          <div className="box">
-            <i className="fas fa-trash"></i>
-            <img src="images/cart-2.png" alt="" />
-            <div className="content">
-              <h3>mocha magic</h3>
-              <span className="price">$34.99/=</span>
-              <span className="quantity"> qty: 1</span>
-            </div>
-          </div>
-          <div className="box">
-            <i className="fas fa-trash"></i>
-            <img src="images/cart-3.png" alt="" />
-            <div className="content">
-              <h3>mocha magic</h3>
-              <span className="price">$34.99/=</span>
-              <span className="quantity"> qty: 1</span>
-            </div>
-          </div>
-          <div className="total">total : $59.99/=</div>
-          <a href="#" className="btn">checkout</a>
+    <section className="menu" id="menu">
+      <h1 className="heading" font="Lobster">"Savor the Flavor: Explore Our Coffee Menu"</h1>
+      <div className="box-container">
+        <div className="box">
+          <img src={img1} alt="" />
+          <center>
+          <h3><i>Mocha</i></h3>
+          <div className="price">270.00 <span>350.00</span></div>
+          </center>
+          <a href="#" className="btn">add to cart</a>
         </div>
-        
-      <section className="menu">
-        <div className="box-container">
-          {/* Place your menu items here */}
+        <div className="box">
+          <img src={img2} alt="" />
+          <center>
+          <h3><i>Cappucino</i></h3>
+          <div className="price">150.00 <span>200.00</span></div>
+          </center>
+          <a href="#" className="btn">add to cart</a>
         </div>
-      </section>
-      <div>
-    </div>
-    </>
+        <div className="box">
+          <img src={img3} alt="" />
+          <center>
+          <h3><i>Cold</i></h3>
+          <div className="price">200.00 <span>250.00</span></div>
+          </center>
+          <a href="#" className="btn">add to cart</a>
+        </div>
+        <div className="box">
+          <img src={img4} alt="" />
+          <center>
+          <h3>Black Coffee</h3>
+          <div className="price">150.00 <span>200.00</span></div>
+          </center>
+          <a href="#" className="btn">add to cart</a>
+        </div>
+        <div className="box">
+          <img src={img5} alt="" />
+          <center>
+          <h3>Chocolate chip cookies</h3>
+          <div className="price">50.00 <span>70.00</span></div>
+          </center>
+          <a href="#" className="btn">add to cart</a>
+        </div>
+        <div className="box">
+          <img src={img6} alt="" />
+          <center>
+          <h3>Butter Bun </h3>
+          <div className="price">80.00 <span>100.00</span></div>
+          </center>
+          <a href="#" className="btn">add to cart</a>
+        </div>
+      </div>
+    </section>
   );
 }
 
